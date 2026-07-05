c56a63d43ad845887ae4385b23ef528536860640
radium213 <radium213@gmail.com>
2026-07-05 18:39:25 +0300

    refactor: remove class field antipattern

--- src/gguf_bench/gguf.py
+++ src/gguf_bench/gguf.py
@@ -1,5 +1,7 @@
 import struct
+import io
 from typing import Any
+
 
 class GGUFParser:
     VERSION = 3
@@ -11,40 +13,42 @@ class GGUFParser:
     def __init__(self, path: str):
         self.path = path
 
-    def _read_type(self) -> (str, int):
-        type_id: int = struct.unpack("<I", self.f.read(4))[0]
+    def _read_type(self, f: io.BufferedReader) -> (str, int):
+        type_id: int = struct.unpack("<I", f.read(4))[0]
         if type_id >= GGUFParser.NTYPES:
             raise TypeError("Unknown metadata type.")
         value_type = GGUFParser.TYPES[type_id]
         value_size = GGUFParser.SIZES[type_id]
         return value_type, value_size
 
-    def _read_string(self) -> str:
-        length: int = struct.unpack("<Q", self.f.read(8))[0]
-        b: bytes = struct.unpack(f"<{length}s", self.f.read(length))[0]
+    def _read_string(self, f: io.BufferedReader) -> str:
+        length: int = struct.unpack("<Q", f.read(8))[0]
+        b: bytes = struct.unpack(f"<{length}s", f.read(length))[0]
         return b.decode("utf-8")
 
-    def _read_array(self) -> list[ValueType]:
-        value_type, value_size = self._read_type()
-        length: int = struct.unpack("<Q", self.f.read(8))[0]
-        return [self._read_value(value_type, value_size) for _ in range(length)]
+    def _read_array(self, f: io.BufferedReader) -> list[ValueType]:
+        value_type, value_size = self._read_type(f)
+        length: int = struct.unpack("<Q", f.read(8))[0]
+        return [self._read_value(f, value_type, value_size) for _ in range(length)]
 
-    def _read_value(self, value_type: str, value_size: int) -> ValueType:
+    def _read_value(
+        self, f: io.BufferedReader, value_type: str, value_size: int
+    ) -> ValueType:
         if value_type == "s":
-            return self._read_string()
+            return self._read_string(f)
         if value_type == "_":
-            return self._read_array()
-        return struct.unpack(f"<{value_type}", self.f.read(value_size))[0]
+            return self._read_array(f)
+        return struct.unpack(f"<{value_type}", f.read(value_size))[0]
 
-    def _read_kv(self) -> (str, ValueType):
-        key = self._read_string()
-        value_type, value_size = self._read_type()
-        value = self._read_value(value_type, value_size)
+    def _read_kv(self, f: io.BufferedReader) -> (str, ValueType):
+        key = self._read_string(f)
+        value_type, value_size = self._read_type(f)
+        value = self._read_value(f, value_type, value_size)
         return key, value
 
-    def _read_header(self) -> (str, int, int, int) | None:
+    def _read_header(self, f: io.BufferedReader) -> (str, int, int, int) | None:
         size = 24
-        header_bytes = self.f.read(size)
+        header_bytes = f.read(size)
         if len(header_bytes) < size:
             return None
         [magic, version, tensor_count, metadata_kv_count] = struct.unpack(
@@ -56,8 +60,7 @@ class GGUFParser:
 
     def read_metadata(self) -> dict[str, Any]:
         with open(self.path, "rb") as f:
-            self.f = f
-            header = self._read_header()
+            header = self._read_header(f)
             if not header:
                 raise ValueError(f"{self.path} is not a GGUF file.")
             [magic, version, tensor_count, metadata_kv_count] = header
@@ -67,13 +70,12 @@ class GGUFParser:
                 )
             metadata = {}
             for _ in range(metadata_kv_count):
-                key, value = self._read_kv()
+                key, value = self._read_kv(f)
                 metadata[key] = value
             return metadata, tensor_count
 
     def is_valid(self) -> bool:
         with open(self.path, "rb") as f:
-            self.f = f
-            if not self._read_header():
+            if not self._read_header(f):
                 return False
             return True
