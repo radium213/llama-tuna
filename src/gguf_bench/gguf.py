0ebfabf15699a70c785a33fb847357de1101f545
radium213 <radium213@gmail.com>
2026-07-16 02:30:37 +0300

    refactor: convert GGUF parser to a function and add error handling

--- src/gguf_bench/gguf.py
+++ src/gguf_bench/gguf.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from dataclasses import dataclass
 from typing import Any
 
+SUPPORTED_VERSIONS = [2, 3]
+VALUE_TYPES = "BbHhIif?s_Qqd"
+VALUE_SIZES = [1, 1, 2, 2, 4, 4, 4, 1, 0, 0, 8, 8, 8]
 
 ValueType = int | float | bool | str | list[Any]
 
@@ -21,94 +24,107 @@ class GGUFMetadata:
     raw: dict[str, ValueType]
 
 
-class GGUFFile:
-    VERSION = 3
-    NTYPES = 13
-    TYPES = "BbHhIif?s_Qqd"
-    SIZES = [1, 1, 2, 2, 4, 4, 4, 1, 0, 0, 8, 8]
+class GGUFParsingError(Exception):
+    """Base GGUF parser exception"""
 
-    def __init__(self, path: str | Path):
-        self.path = path
 
-    def _read_type(self, f: io.BufferedReader) -> tuple[str, int]:
-        type_id: int = struct.unpack("<I", f.read(4))[0]
-        if type_id >= GGUFFile.NTYPES:
-            raise TypeError("Unknown metadata type.")
-        value_type = GGUFFile.TYPES[type_id]
-        value_size = GGUFFile.SIZES[type_id]
-        return value_type, value_size
+class IncompatibleHeader(GGUFParsingError):
+    """Header indicates different file type"""
 
-    def _read_string(self, f: io.BufferedReader) -> str:
-        length: int = struct.unpack("<Q", f.read(8))[0]
-        b: bytes = struct.unpack(f"<{length}s", f.read(length))[0]
-        return b.decode("utf-8")
 
-    def _read_array(self, f: io.BufferedReader) -> list[ValueType]:
-        value_type, value_size = self._read_type(f)
-        length: int = struct.unpack("<Q", f.read(8))[0]
-        return [self._read_value(f, value_type, value_size) for _ in range(length)]
+class IncompatibleVersion(GGUFParsingError):
+    """File version is not supported"""
 
-    def _read_value(
-        self, f: io.BufferedReader, value_type: str, value_size: int
-    ) -> ValueType:
-        if value_type == "s":
-            return self._read_string(f)
-        if value_type == "_":
-            return self._read_array(f)
-        return struct.unpack(f"<{value_type}", f.read(value_size))[0]
 
-    def _read_kv(self, f: io.BufferedReader) -> tuple[str, ValueType]:
-        key = self._read_string(f)
-        value_type, value_size = self._read_type(f)
-        value = self._read_value(f, value_type, value_size)
-        return key, value
+class UnknownDataType(GGUFParsingError):
+    """Encountered unknown type id"""
 
-    def _read_header(self, f: io.BufferedReader) -> tuple[int, int, int] | None:
-        size = 24
-        header_bytes = f.read(size)
-        if len(header_bytes) < size:
-            return None
-        [magic, version, tensor_count, metadata_kv_count] = struct.unpack(
-            "<4sIQQ", header_bytes
+
+class StructuralError(GGUFParsingError):
+    """File structure is invalid"""
+
+
+def _read_type(f: io.BufferedReader) -> tuple[str, int]:
+    type_id: int = struct.unpack("<I", f.read(4))[0]
+    if type_id >= len(VALUE_TYPES):
+        raise UnknownDataType(f"Unknown data type id: {type_id}")
+    value_type = VALUE_TYPES[type_id]
+    value_size = VALUE_SIZES[type_id]
+    return value_type, value_size
+
+
+def _read_string(f: io.BufferedReader) -> str:
+    length: int = struct.unpack("<Q", f.read(8))[0]
+    b: bytes = struct.unpack(f"<{length}s", f.read(length))[0]
+    return b.decode("utf-8")
+
+
+def _read_array(f: io.BufferedReader) -> list[ValueType]:
+    value_type, value_size = _read_type(f)
+    length: int = struct.unpack("<Q", f.read(8))[0]
+    return [_read_value(f, value_type, value_size) for _ in range(length)]
+
+
+def _read_value(f: io.BufferedReader, value_type: str, value_size: int) -> ValueType:
+    if value_type == "s":
+        return _read_string(f)
+    if value_type == "_":
+        return _read_array(f)
+    return struct.unpack(f"<{value_type}", f.read(value_size))[0]
+
+
+def _read_kv(f: io.BufferedReader) -> tuple[str, ValueType]:
+    key = _read_string(f)
+    value_type, value_size = _read_type(f)
+    value = _read_value(f, value_type, value_size)
+    return key, value
+
+
+def _read_header(f: io.BufferedReader) -> tuple[int, int, int]:
+    [magic, version, tensor_count, metadata_kv_count] = struct.unpack(
+        "<4sIQQ", f.read(24)
+    )
+    if magic != b"GGUF":
+        raise IncompatibleHeader(
+            f"Expected file signature: GGUF, but got {magic}"
         )
-        if magic != b"GGUF":
-            return None
-        return version, tensor_count, metadata_kv_count
+    return version, tensor_count, metadata_kv_count
 
-    def _read_metadata(self) -> tuple[dict[str, ValueType], int, int]:
-        with open(self.path, "rb") as f:
-            header = self._read_header(f)
-            if not header:
-                raise ValueError(f"{self.path} is not a GGUF file.")
+
+def _read_metadata(path: str | Path) -> tuple[dict[str, ValueType], int, int]:
+    with open(path, "rb") as f:
+        try:
+            header = _read_header(f)
             [version, tensor_count, metadata_kv_count] = header
+            if version not in SUPPORTED_VERSIONS:
+                raise IncompatibleVersion(
+                    f"Expected GGUF version: {SUPPORTED_VERSIONS}, but got {version}"
+                )
             metadata: dict[str, ValueType] = {}
             for _ in range(metadata_kv_count):
-                key, value = self._read_kv(f)
+                key, value = _read_kv(f)
                 metadata[key] = value
             return metadata, version, tensor_count
+        except (struct.error, UnicodeDecodeError) as e:
+            raise StructuralError("File structure is invalid") from e
 
-    def get_metadata(self):
-        gguf_meta, gguf_version, tensor_count = self._read_metadata()
-        file_type = gguf_meta.get("general.type", "")
-        architecture = gguf_meta.get("general.architecture", "")
-        name = gguf_meta.get("general.name", "")
-        size_label = gguf_meta.get("general.size_label", "")
-        block_count = gguf_meta.get(f"{architecture}.block_count", 0)
-        context_length = gguf_meta.get(f"{architecture}.context_length", 0)
-        return GGUFMetadata(
-            gguf_version,
-            tensor_count,
-            str(file_type),
-            str(architecture),
-            str(name),
-            str(size_label),
-            int(block_count) if isinstance(block_count, int) else 0,
-            int(context_length) if isinstance(context_length, int) else 0,
-            raw=gguf_meta,
-        )
 
-    def is_valid(self) -> bool:
-        with open(self.path, "rb") as f:
-            if not self._read_header(f):
-                return False
-            return True
+def read_gguf_metadata(path: str | Path) -> GGUFMetadata:
+    gguf_meta, gguf_version, tensor_count = _read_metadata(path)
+    file_type = gguf_meta.get("general.type", "")
+    architecture = gguf_meta.get("general.architecture", "")
+    name = gguf_meta.get("general.name", "")
+    size_label = gguf_meta.get("general.size_label", "")
+    block_count = gguf_meta.get(f"{architecture}.block_count", 0)
+    context_length = gguf_meta.get(f"{architecture}.context_length", 0)
+    return GGUFMetadata(
+        gguf_version,
+        tensor_count,
+        str(file_type),
+        str(architecture),
+        str(name),
+        str(size_label),
+        int(block_count) if isinstance(block_count, int) else 0,
+        int(context_length) if isinstance(context_length, int) else 0,
+        raw=gguf_meta,
+    )
